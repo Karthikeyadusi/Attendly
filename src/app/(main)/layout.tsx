d9b3771e2115001e24134dbfce010b9b6e65734a
Karthikeya Dusi <dvskartikeya@gmail.com>
2025-12-08 08:46:01 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,6 +6,8 @@ import Header from "@/components/layout/Header";
 import BottomNav from "@/components/layout/BottomNav";
 import DesktopSidebar from "@/components/layout/DesktopSidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useIsClient } from "@/hooks/useIsClient";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function MainAppLayout({
   children,
@@ -13,6 +15,21 @@ export default function MainAppLayout({
   children: React.ReactNode;
 }) {
   const isMobile = useIsMobile();
+  const isClient = useIsClient();
+
+  if (!isClient) {
+    return (
+      <div className="flex h-screen w-full">
+        <Skeleton className="hidden w-64 flex-shrink-0 md:block" />
+        <div className="flex flex-1 flex-col">
+          <Skeleton className="h-16 w-full" />
+          <div className="flex-1 p-6">
+            <Skeleton className="h-full w-full" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <AppProvider>
