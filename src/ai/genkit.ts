75085c92bf0b102d6359a9c8c50b562a27d71a32
Karthikeya Dusi <dvskartikeya@gmail.com>
2025-12-10 18:19:51 +0000

    ??

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      model: 'gemini-1.5-flash-latest'
+      model: 'gemini-1.5-flash'
     }),
   ],
 });
